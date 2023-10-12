126855928c0fa213b22c1eaec7bedb2584425649
Jose A. Fernandez <fernandezja@gmail.com>
2023-10-12 17:18:08 -0300

    Unidad6: ASP.NET MVC Routing

---
+++ Unidad6/Starwars.Apps.WebAppMvc/Controllers/RedireccionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Starwars.Apps.WebAppMvc.Models;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+
+namespace Starwars.Apps.WebAppMvc.Controllers
+{
+    public class RedireccionController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            return RedirectToAction(controllerName: "Usuario",
+                                    actionName: "Buscar", 
+                                    routeValues: new { nombre = id });
+
+        }
+
+        //public IActionResult Index(string id)
+        //{
+        //    return RedirectToAction(controllerName: "Usuario",
+        //                            actionName: "Buscar",
+        //                            routeValues: new { nombre = id });
+
+        //}
+    }
+}
---
+++ Unidad6/Starwars.Apps.WebAppMvc/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Starwars.Apps.WebAppMvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Buscar(string nombre)
+        {
+            var htmlEncoder = HtmlEncoder.Create();
+            
+            var input = htmlEncoder.Encode(nombre);
+
+            return Content($"<html><body><h1>{input}</h1></body></html>", "text/html");
+
+        }
+    }
+}
