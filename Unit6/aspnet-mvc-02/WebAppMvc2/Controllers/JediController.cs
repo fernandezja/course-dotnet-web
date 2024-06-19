85572d55c94bc9bbedd4d621cfc1c57f261ffb3f
Jose A. Fernandez <fernandezja@gmail.com>
2024-06-18 22:44:50 -0300

    [Unit6][aspnet-mvc-01][aspnet-mvc-02]

---
+++ Unit6/aspnet-mvc-02/WebAppMvc2/Controllers/JediController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvc2.Controllers
+{
+    public class JediController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); //Razor
+        }
+
+        [Route("jedi/jedi-{id:int}.html")]
+        [Route("jedi/jedi_{id}.html")]
+        public IActionResult Detail(int? id)
+        {
+            return View("DetailInternal"); //Razor
+        }
+
+        public IActionResult Demo()
+        {
+            return Content("Jedi Demo");
+        }
+    }
+}
---
+++ Unit6/aspnet-mvc-02/WebAppMvc2/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.Use(async (context, next) =>
+{
+
+    // Do work that can write to the Response.
+    await next.Invoke();
+    // Do logging or other work that doesn't write to the Response.
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();  //wwwroot
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
