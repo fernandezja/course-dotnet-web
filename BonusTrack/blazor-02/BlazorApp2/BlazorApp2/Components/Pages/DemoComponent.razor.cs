3506ffe30237331fe05288f1649ff2d59675642e
Jose A. Fernandez <fernandezja@gmail.com>
2024-09-17 17:43:24 -0300

    [BonusTrack][blazor-02] Blazor

---
+++ BonusTrack/blazor-02/BlazorApp2/BlazorApp2/Components/Pages/DemoComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Components.Pages
+{
+    public partial class DemoComponent
+    {
+        [Parameter]
+        public string Text { get; set; }
+    }
+}
