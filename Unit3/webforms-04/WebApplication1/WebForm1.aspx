b50feb55bf6fe93d2a3de995d81c12574adb21cf
Jose A. Fernandez <fernandezja@gmail.com>
2024-10-28 16:43:18 -0300

    [Unit3] Webforms

---
+++ Unit3/webforms-04/WebApplication1/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" 
+         AutoEventWireup="true" 
+         CodeBehind="WebForm1.aspx.cs" 
+         Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Webform1</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Webform1</h1>
+        </div>
+
+        <h2>Titulo 2</h2>
+
+        <p>
+            <label>Name 1:</label>
+            <input type="text" id="txtName1" value="" runat="server" />
+
+            <br />
+
+            <label>Name 2:</label>
+            <asp:TextBox ID="txtName2" runat="server"></asp:TextBox>
+
+
+            <br />
+            <asp:Button ID="btnEnviar" runat="server" 
+                        Text="Enviar" 
+                        OnClick="btnEnviar_Click" />
+        </p>
+        <p>
+            <asp:ListBox ID="listItems" runat="server" Height="179px" Width="222px"></asp:ListBox>
+        </p>
+        <p>
+            &nbsp;</p>
+
+        
+        Item Seleccionado: 
+        <asp:Label ID="lblItemSeleccionado" runat="server" Text="Label"></asp:Label></form>
+</body>
+</html>
---
+++ Unit3/webforms-04/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                CargarItemsPorDefault();
+            }
+
+            
+        }
+
+        private void CargarItemsPorDefault()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                AddItem($"Item {i}");
+            }
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty( txtName2.Text))
+            {
+                AddItem(txtName2.Text);
+            }
+            if (!string.IsNullOrEmpty(txtName1.Value))
+            {
+                AddItem(txtName1.Value);
+            }
+
+            if (listItems.SelectedItem != null)
+            {
+                lblItemSeleccionado.Text = listItems.SelectedItem.Text;
+            }
+            
+        }
+
+        private void AddItem(string text)
+        {
+            listItems.Items.Add(text);
+        }
+    }
+}
