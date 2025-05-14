0e00c17541fe329ad8eea6f5b2c9842eb673258a
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-05-14 17:46:31 -0300

    [Unit3][mvc-06]

---
+++ Unit3/mvc-06/WebAppMvc6/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvc6.Controllers
+{
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string producto)
+        {
+            var carrito = new WebAppMvc6.Models.Carrito()
+            {
+                Id = 1,
+                Producto = producto
+            };
+
+            //var demo = producto;
+
+            //escribir archivo de texto
+
+            return View(carrito);
+        }
+    }
+}
---
+++ Unit3/mvc-06/WebAppMvc6/Models/Carrito.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppMvc6.Models
+{
+    public class Carrito
+    {
+        public int Id { get; set; }
+        public string Producto { get; set; } = string.Empty;
+    }
+}
