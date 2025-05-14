c7474ed80f956465e6d7b7aa35680c6d21dc7c77
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-05-14 17:46:53 -0300

    [Unit3][webforms-06][webforms-07]

---
+++ Unit3/webforms-07/WebApplication7/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication7
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = $"Hello, {DateTime.Now.ToString("HH:mm:ss")}";
+        }
+    }
+}
